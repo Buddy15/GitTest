975cdebde6bcbbcfaf5c59c1ea053ff29f6d6f26
Buddy15 <learningbuddy15@gmail.com>
2019-06-02 20:22:12 +0530

    Branch 2nd commit

--- GitTest/src/TestClass.java
+++ GitTest/src/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("This is the first print statement");
 		System.out.println("This is the inital change");
-		System.out.println("This is the Local change");
+		System.out.println("This is the 3rd line change");
 		
 
 		
