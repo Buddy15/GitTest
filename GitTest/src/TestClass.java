d73c027fb8d823d2dc21b1f9d087f259a90951aa
Buddy15 <learningbuddy123@gmail.com>
2019-06-02 20:07:44 +0530

    Update TestClass.java

--- GitTest/src/TestClass.java
+++ GitTest/src/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("This is the first print statement");
 		System.out.println("This is the inital change");
+		System.out.println("This is the change in the Github");
 		
 		
 
