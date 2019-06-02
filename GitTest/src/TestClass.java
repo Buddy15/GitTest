ca5b8128102f83bdfa8858a2e818214fc5a76979
Buddy15 <learningbuddy15@gmail.com>
2019-06-02 20:02:12 +0530

    First Commit for the GitTest

---
+++ GitTest/src/TestClass.java
@@ -0,0 +1,12 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is the first print statement");
+		System.out.println("This is the inital change");
+		
+		
+
+	}
+
+}
